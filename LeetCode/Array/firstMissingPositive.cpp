bf4fd2a93d3abf5ffb41c42c03c110d788bf6c81
JotiRoy01 <jotiroygit@gmail.com>
2025-07-15 22:56:37 +0600

    update file associations in settings.json

--- LeetCode/Array/firstMissingPositive.cpp
+++ LeetCode/Array/firstMissingPositive.cpp
@@ -7,7 +7,37 @@ using namespace std;
 class Solution{
     public :
         int firstMissingPositive(vector<int>&nums){
+            int n = nums.size();
+            int small = nums[0];
+            int height = nums[0];
+            for ( int i = 0 ; i<n ; i++){
+                if(small > nums[i]){
+                    small = nums[i];
+                }
+                else if(height<nums[i]){
+                    height = nums[i];
+                }
+            }
             
+            int k = 0;
+            int smp = small;
+            for(int j = small ; j<height ; j++){
+                
+                while(small != nums[k]){
+                    if (smp < small){
+                        smp = small;
+                    }
+
+                }
+                
+
+            }
+            if(small != 1){
+                small = 1;
+            }
+            
+
+
         }
 };
 
