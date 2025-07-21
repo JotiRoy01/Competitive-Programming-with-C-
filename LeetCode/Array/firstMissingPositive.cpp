a5655ec2caaab4b23fcb7d87401109315841aae5
JotiRoy01 <jotiroygit@gmail.com>
2025-07-21 23:08:34 +0600

    complete the firstMissingPositive

--- LeetCode/Array/firstMissingPositive.cpp
+++ LeetCode/Array/firstMissingPositive.cpp
@@ -65,29 +65,28 @@ class Solution{
             //return 1; // or throw an exception if nothing is missing
 
             
-            for (int i = 0; i < n; i++) {
-                while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                    // Swap nums[i] and nums[nums[i] - 1]
+            for(int i = 0; i<n ; i++){
+                while(nums[i] >=1 && nums[i] <=n && nums[nums[i] - 1] != nums[i]){
                     int temp = nums[nums[i] - 1];
                     nums[nums[i] - 1] = nums[i];
                     nums[i] = temp;
                 }
             }
-            for(int i = 0; i<n ; i++){
+
+            for(int i = 0 ; i<n ; i++){
                 if(nums[i] != i + 1){
                     return i + 1;
                 }
             }
-            
-            return n+1;
-        
+            return n + 1;
         }        
+
 };
 
 
 int main(){
     Solution s;
-    vector<int>nums = {20,3,-9, 4, -1, 2, -4, 1, -3};
+    vector<int>nums = {1,2,8,4,5,6,7,11,12,13};
     int a = s.firstMissingPositive(nums);
     cout<<"Solution = "<<a<<endl;
     // ...existing code...
